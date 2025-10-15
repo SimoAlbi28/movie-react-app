dbcad038329abd382fe93deca93b2988c8980a6f
SimoAlbi28 (Simone Albini) <simone.albini28@gmail.com>
2025-10-15 14:38:20 +0200

    feat

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,13 @@
 // Header.tsx
 import './Header.css';
+import Search from './Search';  
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        <div className="logo">MediaApp</div>
-        <form className="search-form">
-          <input 
-            type="text" 
-            className="search-input" 
-            placeholder="Cerca film o serie..." 
-          />
-          <button type="submit" className="search-button">Cerca</button>
-        </form>
+        <div className="logo">MediaApp</div> 
+        <Search/>       
       </div>
     </header>
   );
---
+++ src/components/Search.tsx
@@ -0,0 +1,13 @@
+function Search() {
+  return (
+    <form className="search-form">
+        <input 
+            type="text" 
+            className="search-input" 
+            placeholder="Cerca film o serie..." 
+        />
+            <button type="submit" className="search-button">Cerca</button>
+    </form> 
+)}
+
+export default Search;
